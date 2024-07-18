c2015272ee188f9b1991f51bf34db824144d1f06
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-07-18 12:49:28 +0200

    Automatic command discovery

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,43 @@
-﻿using FFCmd.Commands;
+﻿using System.Text.RegularExpressions;
 
 using Spectre.Console.Cli;
 
-var app = new CommandApp();
-app.Configure(config =>
+static string ConvertToKebabCase(string input)
 {
-    config.AddCommand<ConvertFlac>("convert-to-flac");
-    config.AddCommand<ConvertToMp3>("convert-to-mp3");
-});
+    if (string.IsNullOrEmpty(input))
+    {
+        return string.Empty;
+    }
+    return KebabCaseRegex().Replace(input, "-$1").ToLower();
+}
+
+static void AddCommands(IConfigurator config)
+{
+    var commands = typeof(Program).Assembly.GetTypes()
+        .Where(t => t.IsAssignableTo(typeof(ICommand))
+               && t.IsClass
+               && !t.IsAbstract)
+        .ToList();
+
+    var add = config.GetType().GetMethod(nameof(config.AddCommand));
+
+    if (add == null)
+        throw new InvalidOperationException();
+
+    foreach (var command in commands)
+    {
+        var name = ConvertToKebabCase(command.Name) ?? throw new InvalidOperationException();
+        add.MakeGenericMethod(command).Invoke(config, [name]);
+    }
+}
+
+var app = new CommandApp();
+app.Configure(config => AddCommands(config));
 
 app.Run(args);
+
+partial class Program
+{
+    [GeneratedRegex("(?<!^)([A-Z])")]
+    private static partial Regex KebabCaseRegex();
+}
