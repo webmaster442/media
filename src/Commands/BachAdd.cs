81ce3480160ff7ee569c3562ee12579c1597e590
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-07-27 19:38:16 +0200

    Bach add and remove commands

---
+++ src/Commands/BachAdd.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+using FFCmd.Infrastructure;
+using FFCmd.Infrastructure.BaseCommands;
+
+using Spectre.Console.Cli;
+
+namespace FFCmd.Commands;
+
+internal sealed class BachAdd : BaseBachCommand<BachAdd.Settings>
+{
+    public class Settings : BaseBachSettings
+    {
+        [CommandArgument(0, "<file or file pattern>")]
+        [Description("File or file pattern (eg. *.mp3) to add to the project")]
+        [Required]
+        public string FileToAdd { get; set; } = string.Empty;
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        try
+        {
+            var project = await LoadProject(settings.ProjectName);
+            var files = GetFiles(settings.FileToAdd);
+            int count = project.Files.Count;
+            project.Files.AddRange(files);
+            count = project.Files.Count - count;
+            await SaveProject(settings.ProjectName, project);
+
+            Terminal.GreenText($"Added {count} files to project {settings.ProjectName}");
+
+            return ExitCodes.Success;
+        }
+        catch (Exception e)
+        {
+            Terminal.DisplayException(e);
+            return ExitCodes.Exception;
+        }
+    }
+}
---
+++ src/Commands/BachRemove.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+using FFCmd.Infrastructure;
+using FFCmd.Infrastructure.BaseCommands;
+
+using Spectre.Console.Cli;
+
+namespace FFCmd.Commands;
+
+internal sealed class BachRemove : BaseBachCommand<BachRemove.Settings>
+{
+    public class Settings : BaseBachSettings
+    {
+        [CommandArgument(0, "<file or file pattern>")]
+        [Description("File or file pattern (eg. *.mp3) to remove from the project")]
+        [Required]
+        public string FileToAdd { get; set; } = string.Empty;
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        try
+        {
+            var project = await LoadProject(settings.ProjectName);
+            var files = GetFiles(settings.FileToAdd);
+            int count = 0;
+
+            foreach (var file in files)
+            {
+                if (project.Files.Remove(file))
+                {
+                    count++;
+                }
+            }
+
+            await SaveProject(settings.ProjectName, project);
+
+            Terminal.GreenText($"Removed {count} files from project {settings.ProjectName}");
+
+            return ExitCodes.Success;
+        }
+        catch (Exception e)
+        {
+            Terminal.DisplayException(e);
+            return ExitCodes.Exception;
+        }
+    }
+}
