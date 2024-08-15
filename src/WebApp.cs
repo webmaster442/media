ca666e04a47bf9e369ebeb39e779b3ec0a5cc468
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-08-15 13:56:49 +0200

    Crude web server with ASP.NET

---
+++ src/WebApp.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace Media;
+
+internal sealed class WebApp
+{
+    private readonly WebApplication _app;
+    private readonly int _port;
+
+    public WebApp(int port)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+        builder.WebHost.ConfigureKestrel((context, serverOptions) => serverOptions.ListenAnyIP(port));
+        _app = builder.Build();
+        _port = port;
+    }
+
+    public void AddEmbeddedFile(string requestPath,
+                                string embeddedName,
+                                string mimeType)
+    {
+        if (string.IsNullOrEmpty(requestPath))
+        {
+            throw new ArgumentException($"'{nameof(requestPath)}' cannot be null or empty.", nameof(requestPath));
+        }
+
+        if (string.IsNullOrEmpty(embeddedName))
+        {
+            throw new ArgumentException($"'{nameof(embeddedName)}' cannot be null or empty.", nameof(embeddedName));
+        }
+
+        if (string.IsNullOrEmpty(mimeType))
+        {
+            throw new ArgumentException($"'{nameof(mimeType)}' cannot be null or empty.", nameof(mimeType));
+        }
+
+        _app.MapGet(requestPath, async (HttpContext context) =>
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var data = assembly.GetManifestResourceStream(embeddedName);
+            if (data == null)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("File not found.");
+                return;
+            }
+            context.Response.ContentType = "mimeType";
+            context.Response.ContentLength = data.Length;
+            await data.CopyToAsync(context.Response.Body);
+        });
+    }
+
+    public void AddGetRoute([StringSyntax("Route")] string endpoint, RequestDelegate handler)
+        => _app.MapGet(endpoint, handler);
+
+    public void AddPostRoute([StringSyntax("Route")] string endpoint, RequestDelegate handler)
+        => _app.MapPost(endpoint, handler);
+
+    public async Task RunAsync(CancellationToken token)
+        => await _app.RunAsync(token);
+}
