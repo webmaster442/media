27b8bfe4765a138573de0adcfabcdee2465b8353
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-08-21 15:19:09 +0200

    Nuget update

---
+++ src/Dto/TagData.cs
@@ -0,0 +1,16 @@
+﻿namespace Media.Dto;
+
+public sealed class TagData
+{
+    public required string FileName { get; init; }
+    public required string Title { get; init; }
+    public required string Artist { get; init; }
+    public required string Album { get; init; }
+    public required string Genre { get; init; }
+    public required string Year { get; init; }
+
+    public string Comment { get; init; } = string.Empty;
+    public string AlbumArtist { get; init; } = string.Empty;
+    public string Composer { get; init; } = string.Empty;
+    public string Discnumber { get; init; } = string.Empty;
+}
