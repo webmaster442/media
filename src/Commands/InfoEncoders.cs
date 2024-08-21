686a29e87408fec655153d403d54a2f4ca4c2384
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-08-21 16:00:12 +0200

    Encoder info command progress

---
+++ src/Commands/InfoEncoders.cs
@@ -0,0 +1,97 @@
+﻿
+using Media.Dto.Internals;
+using Media.Infrastructure;
+using Media.Interop;
+
+namespace Media.Commands;
+
+internal class InfoEncoders : AsyncCommand<InfoEncoders.Settings>
+{
+    public class Settings : ValidatedCommandSettings
+    {
+        [CommandOption("-a|--audio")]
+        [Description("List audio encoders")]
+        public bool ListAudio { get; set; }
+
+        [CommandOption("-v|--video")]
+        [Description("List video encoders")]
+        public bool ListVideo { get; set; }
+
+        [CommandOption("-s|--subtitle")]
+        [Description("List subtitle encoders")]
+        public bool ListSubtitle { get; set; }
+
+        public bool NoneGiven =>
+            !ListAudio && !ListVideo && !ListSubtitle;
+    }
+
+    private readonly ConfigAccessor _configAccessor;
+
+    public InfoEncoders()
+    {
+        _configAccessor = new ConfigAccessor();
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        var encoders = await GetEncoders();
+        if (!settings.NoneGiven)
+        {
+            encoders = Filter(encoders, settings);
+        }
+        PrintTable(encoders);
+        return ExitCodes.Success;
+    }
+
+    private FFMpegEncoderInfo[] Filter(FFMpegEncoderInfo[] encoders, Settings settings)
+    {
+        var filter = PredicateBuilder.False<FFMpegEncoderInfo>();
+        if (settings.ListVideo)
+        {
+            filter = filter.Or(e => e.Type == FFMpegEncoderInfo.EncoderType.Video);
+        }
+        if (settings.ListAudio)
+        {
+            filter = filter.Or(e => e.Type == FFMpegEncoderInfo.EncoderType.Audio);
+        }
+        if (settings.ListSubtitle)
+        {
+            filter = filter.Or(e => e.Type == FFMpegEncoderInfo.EncoderType.Subtitle);
+        }
+        return encoders
+            .Where(filter.Lambda())
+            .ToArray();
+    }
+
+    private void PrintTable(FFMpegEncoderInfo[] encoders)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task<FFMpegEncoderInfo[]> GetEncoders()
+    {
+        var installedVersion = _configAccessor.GetInstalledVersion("ffmpeg");
+        if (installedVersion == null)
+            throw new InvalidOperationException("Coudn't get installed version of ffmpeg");
+
+        var cached = _configAccessor.GetCachedEncoderList();
+        if (cached != null 
+            && installedVersion == cached.Version)
+        {
+            return cached.Encoders;
+        }
+
+        var encoderString = FFMpeg.GetEnoderList();
+        
+        var parsed = Parsers.ParseEncoderInfos(encoderString).ToArray();
+
+        await _configAccessor.SetCachedEncoderList(new Dto.Config.EncoderInfos
+        { 
+            Encoders = parsed,
+            Version = installedVersion.Value,
+        });
+
+        return parsed;
+
+    }
+}
---
+++ src/Infrastructure/PredicateBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace Media.Infrastructure;
+
+internal static class PredicateBuilder
+{
+    public static Expression<Func<T, bool>> True<T>() => _ => true;
+
+    public static Expression<Func<T, bool>> False<T>() => _ => false;
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        InvocationExpression invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        InvocationExpression invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+
+    public static Func<T, bool> Lambda<T>(this Expression<Func<T, bool>> expr)
+        => expr.Compile();
+}
