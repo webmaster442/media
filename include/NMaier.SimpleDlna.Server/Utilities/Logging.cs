7e354e02f4837ed98314077b9e5134c75090cd2f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-10-05 09:52:44 +0200

    Fix nullability annotation of Logger after Nlog update

--- include/NMaier.SimpleDlna.Server/Utilities/Logging.cs
+++ include/NMaier.SimpleDlna.Server/Utilities/Logging.cs
@@ -23,17 +23,17 @@ public class Logging : ILog
 
     public ILogger Logger => InternalLogger.Logger;
 
-    public void Debug(object message)
+    public void Debug(object? message)
     {
         InternalLogger.Debug(message);
     }
 
-    public void Debug(object message, Exception exception)
+    public void Debug(object? message, Exception? exception)
     {
         InternalLogger.Debug(message, exception);
     }
 
-    public void DebugFormat(string format, params object[] args)
+    public void DebugFormat(string format, params object?[]? args)
     {
         InternalLogger.DebugFormat(format, args);
     }
@@ -48,8 +48,7 @@ public class Logging : ILog
         InternalLogger.DebugFormat(format, arg0, arg1);
     }
 
-    public void DebugFormat(IFormatProvider provider, string format,
-      params object[] args)
+    public void DebugFormat(IFormatProvider? provider, string format, params object?[]? args)
     {
         InternalLogger.DebugFormat(provider, format, args);
     }
@@ -60,150 +59,142 @@ public class Logging : ILog
         InternalLogger.DebugFormat(format, arg0, arg1, arg2);
     }
 
-    public void Error(object message)
+    public void Error(object? message)
     {
         InternalLogger.Error(message);
     }
 
-    public void Error(object message, Exception exception)
+    public void Error(object? message, Exception? exception)
     {
         InternalLogger.Error(message, exception);
     }
 
-    public void ErrorFormat(string format, params object[] args)
+    public void ErrorFormat(string format, params object?[]? args)
     {
         InternalLogger.ErrorFormat(format, args);
     }
 
-    public void ErrorFormat(string format, object arg0)
+    public void ErrorFormat(string format, object? arg0)
     {
         InternalLogger.ErrorFormat(format, arg0);
     }
 
-    public void ErrorFormat(string format, object arg0, object arg1)
+    public void ErrorFormat(string format, object? arg0, object? arg1)
     {
         InternalLogger.ErrorFormat(format, arg0, arg1);
     }
 
-    public void ErrorFormat(IFormatProvider provider, string format,
-      params object[] args)
+    public void ErrorFormat(IFormatProvider? provider, string format, params object?[]? args)
     {
         InternalLogger.ErrorFormat(provider, format, args);
     }
 
-    public void ErrorFormat(string format, object arg0, object arg1,
-      object arg2)
+    public void ErrorFormat(string format, object? arg0, object? arg1, object? arg2)
     {
         InternalLogger.ErrorFormat(format, arg0, arg1, arg2);
     }
 
-    public void Fatal(object message)
+    public void Fatal(object? message)
     {
         InternalLogger.Fatal(message);
     }
 
-    public void Fatal(object message, Exception exception)
+    public void Fatal(object? message, Exception? exception)
     {
         InternalLogger.Fatal(message, exception);
     }
 
-    public void FatalFormat(string format, params object[] args)
+    public void FatalFormat(string format, params object?[]? args)
     {
         InternalLogger.FatalFormat(format, args);
     }
 
-    public void FatalFormat(string format, object arg0)
+    public void FatalFormat(string format, object? arg0)
     {
         InternalLogger.FatalFormat(format, arg0);
     }
 
-    public void FatalFormat(string format, object arg0, object arg1)
+    public void FatalFormat(string format, object? arg0, object? arg1)
     {
         InternalLogger.FatalFormat(format, arg0, arg1);
     }
 
-    public void FatalFormat(IFormatProvider provider, string format,
-      params object[] args)
+    public void FatalFormat(IFormatProvider? provider, string format, params object?[]? args)
     {
         InternalLogger.FatalFormat(provider, format, args);
     }
 
-    public void FatalFormat(string format, object arg0, object arg1,
-      object arg2)
+    public void FatalFormat(string format, object? arg0, object? arg1, object? arg2)
     {
         InternalLogger.FatalFormat(format, arg0, arg1, arg2);
     }
 
-    public void Info(object message)
+    public void Info(object? message)
     {
         InternalLogger.Info(message);
     }
 
-    public void Info(object message, Exception exception)
+    public void Info(object? message, Exception? exception)
     {
         InternalLogger.Info(message, exception);
     }
 
-    public void InfoFormat(string format, params object[] args)
+    public void InfoFormat(string format, params object?[]? args)
     {
         InternalLogger.InfoFormat(format, args);
     }
 
-    public void InfoFormat(string format, object arg0)
+    public void InfoFormat(string format, object? arg0)
     {
         InternalLogger.InfoFormat(format, arg0);
     }
 
-    public void InfoFormat(string format, object arg0, object arg1)
+    public void InfoFormat(string format, object? arg0, object? arg1)
     {
         InternalLogger.InfoFormat(format, arg0, arg1);
     }
 
-    public void InfoFormat(IFormatProvider provider, string format,
-      params object[] args)
+    public void InfoFormat(IFormatProvider? provider, string format, params object?[]? args)
     {
         InternalLogger.InfoFormat(provider, format, args);
     }
 
-    public void InfoFormat(string format, object arg0, object arg1,
-      object arg2)
+    public void InfoFormat(string format, object? arg0, object? arg1, object? arg2)
     {
         InternalLogger.InfoFormat(format, arg0, arg1, arg2);
     }
 
-    public void Warn(object message)
+    public void Warn(object? message)
     {
         InternalLogger.Warn(message);
     }
 
-    public void Warn(object message, Exception exception)
+    public void Warn(object? message, Exception? exception)
     {
         InternalLogger.Warn(message, exception);
     }
 
-    public void WarnFormat(string format, params object[] args)
+    public void WarnFormat(string format, params object?[]? args)
     {
         InternalLogger.WarnFormat(format, args);
     }
 
-    public void WarnFormat(string format, object arg0)
+    public void WarnFormat(string format, object? arg0)
     {
         InternalLogger.WarnFormat(format, arg0);
     }
 
-    public void WarnFormat(string format, object arg0, object arg1)
+    public void WarnFormat(string format, object? arg0, object? arg1)
     {
         InternalLogger.WarnFormat(format, arg0, arg1);
     }
 
-    public void WarnFormat(IFormatProvider provider, string format,
-      params object[] args)
+    public void WarnFormat(IFormatProvider? provider, string format, params object?[]? args)
     {
         InternalLogger.WarnFormat(provider, format, args);
     }
 
-    public void WarnFormat(string format, object arg0, object arg1,
-      object arg2)
+    public void WarnFormat(string format, object? arg0, object? arg1, object? arg2)
     {
         InternalLogger.WarnFormat(format, arg0, arg1, arg2);
     }
