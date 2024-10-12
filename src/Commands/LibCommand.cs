210a6f222d3f985cabec8e64f82717870675a8b7
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-10-12 19:15:53 +0200

    Media lib progress

---
+++ src/Commands/LibCommand.cs
@@ -0,0 +1,13 @@
+﻿using Media.Infrastructure.BaseCommands;
+using Media.Interfaces;
+using Media.Ui;
+
+namespace Media.Commands;
+
+internal sealed class LibCommand : GuiCommandBase<LibWindow>
+{
+    protected override IViewModel? CreateDataContext(IUiFunctions uiFunctions)
+    {
+        return new LibWindowViewModel();
+    }
+}
---
+++ src/Ui/LibWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using Media.Interfaces;
+
+namespace Media.Ui;
+
+internal sealed partial class LibWindowViewModel : ObservableObject, IViewModel
+{
+    [ObservableProperty]
+    private object _contentItem;
+
+    public LibWindowViewModel()
+    {
+        _contentItem = new object();
+    }
+
+    public void Initialize()
+    {
+    }
+}
