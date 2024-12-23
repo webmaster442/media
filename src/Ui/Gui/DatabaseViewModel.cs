c7b194a57c2763d9611cf8e928734ea87bddd2f9
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-12-23 15:44:33 +0100

    DatabaseView Boilerplate code

---
+++ src/Ui/Gui/DatabaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Media.Ui.Gui;
+
+internal class DatabaseViewModel : ObservableObject
+{
+}
