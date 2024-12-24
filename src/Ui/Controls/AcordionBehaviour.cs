aea8b7342799e3f1c78bff995bf1eeca57fbf15a
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-12-24 20:49:00 +0100

    Acordion for db view

---
+++ src/Ui/Controls/AcordionBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Media.Ui.Controls;
+
+internal sealed class AcordionBehaviour
+{
+    public static List<Expander> _expanders = new();
+
+    public static readonly DependencyProperty IsAcordionGroupProperty =
+        DependencyProperty.RegisterAttached("IsAcordionGroup", typeof(bool), typeof(Expander), new PropertyMetadata(false, OnChange));
+
+    public static bool GetIsAcordionGroup(DependencyObject obj)
+        => (bool)obj.GetValue(IsAcordionGroupProperty);
+
+    public static void SetIsAcordionGroup(DependencyObject obj, bool value)
+        => obj.SetValue(IsAcordionGroupProperty, value);
+
+    private static void OnChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Expander expander)
+            return;
+
+        if (e.NewValue is false)
+        {
+            expander.Expanded -= OnExpanderExpand;
+            _expanders.Remove(expander);
+        }
+        else
+        {
+            expander.Expanded += OnExpanderExpand;
+            _expanders.Add(expander);
+        }
+    }
+
+    private static void OnExpanderExpand(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Expander expander)
+            return;
+        foreach (var item in _expanders)
+        {
+            if (item != expander)
+                item.IsExpanded = false;
+        }
+    }
+}
